6bf29e6200961ab46fad282e042925d6d5153dce
ThomasNGit <neronithomas1989@gmail.com>
2020-01-23 19:06:53 +0100

    fixed the migrations

--- database/migrations/2020_01_23_175848_create_posts_table.php
+++ database/migrations/2020_01_23_175848_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddForeign extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,10 @@ class AddForeign extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-
-            $table -> bigInteger('category_id') -> unsigned() -> index();
-            $table -> foreign('category_id', 'post_category')
-                   -> references('id')
-                   -> on('categories'); 
+            $table -> bigIncrements('id');
+            $table -> string('title');
+            $table -> text('body');
+            $table -> timestamps();
         });
     }
 
@@ -29,6 +28,6 @@ class AddForeign extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('posts');
     }
 }
