2a36f162a7527ff664b24a584e6d92b7f25a383e
ThomasNGit <neronithomas1989@gmail.com>
2020-01-23 20:15:29 +0100

    added components/layouts/pages

--- public/js/app.js
+++ public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\thomas\desktop\boolean\backend\laravel\laravel-mail-auth\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\thomas\desktop\boolean\backend\laravel\laravel-mail-auth\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Thomas\Desktop\Boolean\BackEnd\Laravel\laravel-mail-auth\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Thomas\Desktop\Boolean\BackEnd\Laravel\laravel-mail-auth\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
